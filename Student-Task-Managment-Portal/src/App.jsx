6642d154935c1aa79a1552ff5eacef59a7e17754
sujan-200 <ganigasujan@gmail.com>
2026-09-08 16:27:41 +0530

    Add student task management portal

---
+++ Student-Task-Managment-Portal/src/App.jsx
@@ -0,0 +1,9 @@
+import Welcome from "./components/Welcome";
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  );
+}
+export default App;
---
+++ Student-Task-Managment-Portal/src/components/Welcome.jsx
@@ -0,0 +1,5 @@
+function Welcome() {
+    return (<h1>Welcome to To-Do List</h1>);
+
+}
+export default Welcome;
