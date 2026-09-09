7a4be6c44f07d2fbba90d686daa9147786de8972
sujan-200 <ganigasujan@gmail.com>
2026-09-09 16:09:57 +0530

    Update project structure and styling for student task management portal

--- Student-Task-Managment-Portal/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Welcome from "./components/Welcome";
-function App() {
-  return (
-    <div>
-      <Welcome />
-    </div>
-  );
-}
-export default App;
--- Student-Task-Managment-Portal/src/components/Welcome.jsx
+++
@@ -1,5 +0,0 @@
-function Welcome() {
-    return (<h1>Welcome to To-Do List</h1>);
-
-}
-export default Welcome;
